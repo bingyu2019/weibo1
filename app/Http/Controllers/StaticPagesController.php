11a709bf1860ee92733b060023ecd7dcab688bc5
vagrant <vagrant@homestead>
2019-10-13 15:09:38 +0000

    删除微博

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -16,7 +16,7 @@ class StaticPagesController
             $feed_items = Auth::user()->feed()->paginate(30);
         }
 
-        return view('static_pages/home', compact('feed_items'));
+        return view('static_pages.home', compact('feed_items'));
     }
 
     public function about()
