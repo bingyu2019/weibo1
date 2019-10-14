fde9559095d12d7f93ea5103fa13444d80ab4d6d
vagrant <vagrant@homestead>
2019-10-14 08:28:36 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,29 +11,14 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
@@ -64,7 +49,10 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+
+        return Status::whereIn('user_id', $user_ids)->with('user')
             ->orderBy('created_at', 'desc');
     }
 
